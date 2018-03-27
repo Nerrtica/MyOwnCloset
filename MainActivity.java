d77c0ac131e1b53093af7e1e962542330d5ac013
ladma <kdhpope@naver.com>
2018-03-27 17:46:33 +0900

    client

--- MainActivity.java
+++ MainActivity.java
@@ -7,32 +7,40 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button sendData;
+    private Button sendDataButton;
     private EditText inputText;
     private TextView outputText;
-    final int PORT = 1573;
-    final String IP = "192.168.1.57";
+    final int PORT = 11559;
+    final String IP = "10.210.60.37";
+    DataInputStream is;
+    DataOutputStream os;
+    public SendData connectionServer;
+    String message="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sendData = (Button)findViewById(R.id.sendButton);
+        sendDataButton = (Button)findViewById(R.id.sendButton);
         inputText = (EditText) findViewById(R.id.TextField);
         outputText = (TextView)findViewById(R.id.outputField);
 
-        sendData.setOnClickListener(new View.OnClickListener(){
+
+        sendDataButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-
+                 connectionServer= new SendData();
+                 connectionServer.start();
             }
         });
     }
@@ -40,12 +48,35 @@ public class MainActivity extends AppCompatActivity {
         public void run(){
             try{
                 Socket clientScoekt = new Socket(IP,PORT);
-                clientScoekt.bind();
+
+                is=new DataInputStream(clientScoekt.getInputStream());
+                os=new DataOutputStream(clientScoekt.getOutputStream());
 
             } catch (IOException e) {
                 System.out.println("ERROR");
                 e.printStackTrace();
             }
+
+            message = inputText.getText().toString();
+
+            try {
+                os.writeUTF(message);
+                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                message = is.readUTF();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    outputText.setText(message);
+                }
+            });
+
         }
     }
 }
