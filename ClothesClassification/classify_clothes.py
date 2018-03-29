4418ddd1008eeeac66ebdb3fd072f553882679c0
Nerrtica <nerrtica@naver.com>
2018-03-29 16:17:16 +0900

    기본적인 CNN 모델 생성 함수 작성

--- ClothesClassification/classify_clothes.py
+++ ClothesClassification/classify_clothes.py
@@ -8,3 +8,28 @@ def get_dataset(train_folder, test_folder):
 	train_set = train_datagen.flow_from_directory(train_folder, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
 	test_set = test_datagen.flow_from_directory(test_folder, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
 	return train_set, test_set
+
+def generate_model():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(64, 64, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+
+	model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+	model.add(Conv2D(64, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+
+	model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+	model.add(Conv2D(64, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+
+	model.add(Flatten())
+	model.add(Dense(512, activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(15, activation='softmax'))
+
+	model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
