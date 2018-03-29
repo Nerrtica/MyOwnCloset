e0d82167ba457c1117178ddd51cd6595be91fb5e
Nerrtica <nerrtica@naver.com>
2018-03-29 16:15:15 +0900

    데이터를 flow_from_directory 메소드를 이용해 읽어오도록 변경

--- ClothesClassification/classify_clothes.py
+++ ClothesClassification/classify_clothes.py
@@ -1,42 +1,10 @@
-import sys
-import json
-import numpy as np
-import matplotlib.pyplot as plt
-import keras
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
-from keras.utils import to_categorical
-from PIL import Image
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.preprocessing.image import ImageDataGenerator
 
-def read_data(l, c, size):
-	links = []
-	labels = []
-	with open(l) as f:
-	    count = 0
-	    prev = -1
-	    for line in f:
-	        category = int(line.split('/')[0])
-	        if category == prev:
-	            count += 1
-	        else:
-	            count = 0
-	            prev = category
-	        if c != 0 and count >= c:
-	            continue
-	        links.append('ACS/images/'+line.strip()+'.jpg')
-	        labels.append(category)
-	labels = np.array(labels)
-
-	data = []
-	for i, link in enumerate(links):
-	    if (c != 0 and i % c == 0) or (c == 0 and i % 10000 == 0):
-	        sys.stdout.write('\rProcessing ... %d' % i)
-	    img = Image.open(link)
-		arr = np.array(img.resize(size))
-	    data.append(arr)
-	data = np.array(data)
-
-	return (links, data, labels)
-
-train_links, train_data, train_labels = read_data('ACS/train.txt', 100, (256, 256))
-test_links, test_data, test_labels = read_data('ACS/test.txt', 100, (256, 256))
+def get_dataset(train_folder, test_folder):
+	train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+	test_datagen = ImageDataGenerator(rescale = 1./255)
+	train_set = train_datagen.flow_from_directory(train_folder, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+	test_set = test_datagen.flow_from_directory(test_folder, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+	return train_set, test_set
