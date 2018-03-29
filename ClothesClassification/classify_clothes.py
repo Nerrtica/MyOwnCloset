7a589b8ad8e4f29c89724fedc14221136085acbf
Nerrtica <nerrtica@naver.com>
2018-03-29 16:20:43 +0900

    target size와 input shape, category 개수를 parameter로 받도록 수정

--- ClothesClassification/classify_clothes.py
+++ ClothesClassification/classify_clothes.py
@@ -2,16 +2,16 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 
-def get_dataset(train_folder, test_folder):
+def get_dataset(train_folder, test_folder, target_size):
 	train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
 	test_datagen = ImageDataGenerator(rescale = 1./255)
-	train_set = train_datagen.flow_from_directory(train_folder, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
-	test_set = test_datagen.flow_from_directory(test_folder, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+	train_set = train_datagen.flow_from_directory(train_folder, target_size = target_size, batch_size = 32, class_mode = 'categorical')
+	test_set = test_datagen.flow_from_directory(test_folder, target_size = target_size, batch_size = 32, class_mode = 'categorical')
 	return train_set, test_set
 
-def generate_model():
+def generate_model(input_shape, category_num):
 	model = Sequential()
-	model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(64, 64, 3)))
+	model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
 	model.add(Conv2D(32, (3, 3), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.25))
@@ -29,7 +29,7 @@ def generate_model():
 	model.add(Flatten())
 	model.add(Dense(512, activation='relu'))
 	model.add(Dropout(0.5))
-	model.add(Dense(15, activation='softmax'))
+	model.add(Dense(category_num, activation='softmax'))
 
 	model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 	return model
