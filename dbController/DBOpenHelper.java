546650ecdeac53249237e5a466b2904f61277343
kdhpope <kdhpope@naver.com>
2018-04-04 17:01:27 +0900

    Update DBOpenHelper.java

--- dbController/DBOpenHelper.java
+++ dbController/DBOpenHelper.java
@@ -14,11 +14,12 @@ public class DBOpenHelper extends SQLiteOpenHelper{
         super(context, name, factory, version);
     }
 
+    //make table, execute once when app is installed
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE closet (_id INTEGER PRIMARY KEY AUTOINCREMENT, type CHAR(10), color CHAR(15), image BLOB, FOREIGN KEY (type) REFERENCES clothes_type(section));");
         db.execSQL("CREATE TABLE clothes_type (category CHAR(10),section CHAR(20), PRIMARY KEY(category,section));");
-        db.execSQL("CREATE TABLE coordi  (_id INTEGER PRIMARY KEY AUTOINCREMENT, name CHAR(20), top_clothes TEXT, botton_clothes TEXT);");
+        db.execSQL("CREATE TABLE coordi  (_id INTEGER PRIMARY KEY AUTOINCREMENT, name CHAR(20), top_clothes ChAR(10), botton_clothes CHAR(10));");
         //Toast.makeText(context,"DB is opened",0).show();
     }
 
