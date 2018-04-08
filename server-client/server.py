758fd0d352ad35424fa1baf9605b5bc00fd8b3f6
kdhpope <kdhpope@naver.com>
2018-04-08 15:31:09 +0900

    Update server.py
    server.py UDP -> TCP

--- server-client/server.py
+++ server-client/server.py
@@ -1,24 +1,27 @@
 from socket import *
 import UpperSentence
+import threading
+from _thread import *
+
+def clientSocket(connectionSocket,num):
+	originMessage = connectionSocket.recv(2048)
+	decodedmessage = originMessage.decode()
+
+	UpperData = UpperSentence.UpperSentence(decodedmessage)
+	connectionSocket.send(UpperData.encode())
 
 HOST = ''
 PORT = 11559
 ADDR = (HOST,PORT)
+BUFSIZE = 2048
 
-ServerSocket = socket(AF_INET,SOCK_DGRAM)
+
+ServerSocket = socket(AF_INET,SOCK_STREAM)
 ServerSocket.bind(('',11559))
-
-
-connection_list = [ServerSocket]
+ServerSocket.listen(10)
 
 while True :
 
-	
-    message, clientAddress = ServerSocket.recvfrom(2048)
-
-    data = message.decode()
-    print(data)
-    UpperData = UpperSentence.UpperSentence(data);
-    print(UpperData)
-    ServerSocket.sendto(UpperData.encode(), clientAddress)
-
+	(connectionSocket, clientAddress) = ServerSocket.accept()
+	num = 0
+	start_new_thread(clientSocket,(connectionSocket,num))
