ca7e16535aa4498902b6b7b874f82a15b8163108
kdhpope <kdhpope@naver.com>
2018-04-12 20:48:45 +0900

    Update MainActivity.java

--- dbController/MainActivity.java
+++ dbController/MainActivity.java
@@ -1,14 +1,16 @@
-
-package kacaumap.kacau.com.db;
+package com.example.caucse.db;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DBName = "testDB.db";
@@ -17,39 +19,39 @@ public class MainActivity extends AppCompatActivity {
     EditText section;
     Button SubmitButton;
     Button loadButton;
+    ImageView imageView;
+    DBcontroller controller ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        openHelper = new DBOpenHelper(getApplicationContext(),DBName,null,1);
-
+        controller = new DBcontroller(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         category = (EditText)findViewById(R.id.category);
         section = (EditText)findViewById(R.id.section);
-
+        imageView = (ImageView)findViewById(R.id.imageView);
         SubmitButton = (Button)findViewById(R.id.submit);
         loadButton = (Button)findViewById(R.id.load);
+
+        final Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.neete);
+        imageView.setImageBitmap(image);
 
         SubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SQLiteDatabase db = openHelper.getWritableDatabase();
-                String sql = "INSERT INTO clothes_type VALUES ('" + category.getText().toString() +"', '"+ section.getText().toString()+"');";
-                db.execSQL(sql);
+                controller.InsertCloset(category.getText().toString(),section.getText().toString(),image);
                 category.setText("");
                 section.setText("");
+                imageView.setImageBitmap(null);
             }
         });
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SQLiteDatabase db = openHelper.getReadableDatabase();
-                Cursor cursor = db.rawQuery("select * from clothes_type",null);
-                if(cursor.moveToNext()){
-                    category.setText(cursor.getString(0).toString());
-                    section.setText(cursor.getString(1).toString());
-                }
-
+                Closet name = controller.FindCloset()[0];
+                category.setText(name.getType());
+                section.setText(name.getColor());
+                imageView.setImageBitmap(name.getImage());
             }
         });
     }
