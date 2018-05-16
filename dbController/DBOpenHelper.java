3442a09cf5b401a5a698ee64f06d6df1f93ad55f
kdhpope <kdhpope@naver.com>
2018-05-16 17:18:24 +0900

    Update DBOpenHelper.java

--- dbController/DBOpenHelper.java
+++ dbController/DBOpenHelper.java
@@ -3,7 +3,6 @@ package com.example.caucse.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by ladma on 2018-04-03.
@@ -17,10 +16,25 @@ public class DBOpenHelper extends SQLiteOpenHelper{
     //make table, execute once when app is installed
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE closet (_id INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER,pattern CHAR(10), color CHAR(15), image BLOB, FOREIGN KEY (type) REFERENCES clothes_type(_id));");
-        db.execSQL("CREATE TABLE clothes_type (_id INTEGER PRIMARY KEY AUTOINREMENT, category CHAR(10),section CHAR(20));");
+        db.execSQL("CREATE TABLE closet (_id INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, pattern TEXT, " +
+                "color TEXT, image TEXT, FOREIGN KEY (type) REFERENCES clothes_type(_id));");
+        db.execSQL("CREATE TABLE clothes_type (_id INTEGER PRIMARY KEY AUTOINCREMENT, category CHAR(10),section CHAR(20));");
         db.execSQL("CREATE TABLE coordi  (_id INTEGER PRIMARY KEY AUTOINCREMENT, name CHAR(20), top_clothes ChAR(10), botton_clothes CHAR(10));");
         //Toast.makeText(context,"DB is opened",0).show();
+        
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'outerWear','jacket');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'outerWear','coat');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'outerWear','hoody');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'top','hoody');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'top','sweater');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'top','shirt');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'top','Tshirt');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'bottom','shorts');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'bottom','jeans');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'bottom','pants');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'shoes','sneaker');
+        db.execSQL("INSERT INTO clothes_type VALUES(NULL, 'shoes','shoes');
+        
     }
 
     @Override
